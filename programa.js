cc0124350b3238eee65e49b53030174f23f42d83
Erich Queiroz <95364468+erich-queiroz@users.noreply.github.com>
2021-12-29 22:19:27 -0300

    Add files via upload

---
+++ programa.js
@@ -0,0 +1,24 @@
+function somarValores(){
+    let n1 = document.getElementById("v1").value;
+    let n2 = document.getElementById("v2").value;
+    let n3 = parseInt(n1) + parseInt(n2);
+        alert(n3);
+}
+function subtrairValores(){
+    let n1 = document.getElementById("v1").value;
+    let n2 = document.getElementById("v2").value;
+    let n3 = parseInt(n1) - parseInt(n2);
+        alert(n3);
+}
+function multiplicarValores(){
+    let n1 = document.getElementById("v1").value;
+    let n2 = document.getElementById("v2").value;
+    let n3 = parseInt(n1) * parseInt(n2);
+        alert(n3);
+}
+function dividirValores(){
+    let n1 = document.getElementById("v1").value;
+    let n2 = document.getElementById("v2").value;
+    let n3 = parseInt(n1) / parseInt(n2);
+        alert(n3);
+}
